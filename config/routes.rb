0a0074debfd119b1544deb30ddbd0f53d95dbe85
joemecha <jwmecha@gmail.com>
2023-07-03 14:36:29 -0600

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v0 do
+      resources :gardens, only: %i[index show create update]
+      resources :locations, only: %i[index show create update]
+      resources :crops, only: %i[index show create update destroy]
+      resources :notes, only: %i[index show create update destroy]
+    end
+  end
 end
