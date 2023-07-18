836dbae25af1b40af1f70380da49faee2151a4ad
joemecha <jwmecha@gmail.com>
2023-07-18 16:39:51 -0600

    Add WIP or TODO for user, garden factories and garden show request tests

--- spec/factories/garden.rb
+++ spec/factories/garden.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :garden do
     name { "#{Faker::Lorem.words(number: 2)} Garden" }
+    # TODO: add size
+
+    # TODO: add traits
   end
 end
---
+++ spec/requests/api/v0/gardens/show_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe 'Garden Index', type: :request do
+end
