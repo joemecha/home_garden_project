eb98811d89cf5bfed81182ea5690899ccb9a5f25
joemecha <jwmecha@gmail.com>
2023-07-18 16:35:12 -0600

    Add gardens controller with index and show actions

--- app/controllers/api/v0/gardens_controller.rb
+++ app/controllers/api/v0/gardens_controller.rb
@@ -1,12 +1,24 @@
 class GardensController < ApplicationController
+  before_action :set_garden, only: :show
   def index
     if Garden.all.none?
       render json: { message: 'You currently have no gardens.'}
     else
-      render json: GardenSerializer.new()
+      render json: GardenSerializer.new(Garden.all)
     end
   end
 
   def show
+    if @garden.nil?
+      render json: { errors: "No garden with ID #{params[:id]}" }, status: :not_found
+    else
+      render json: GardenSerializer.new(@garden)
+    end
+  end
+
+  private
+
+  def set_garden
+    @garden = Garden.find(params[:id])
   end
 end
