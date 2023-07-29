e8845fffe0c56c03a772a57460e20396f90e269c
joemecha <jwmecha@gmail.com>
2023-07-29 16:03:31 -0600

    Scope garden index to current user

--- app/controllers/api/v0/gardens_controller.rb
+++ app/controllers/api/v0/gardens_controller.rb
@@ -1,12 +1,40 @@
-class GardensController < ApplicationController
+# frozen_string_literal: true
+
+class Api::V0::GardensController < Api::V0::BaseController
+  before_action :set_garden, only: %i[show]
+
   def index
-    if Garden.all.none?
+    if @current_user.gardens.none?
       render json: { message: 'You currently have no gardens.'}
     else
-      render json: GardenSerializer.new()
+      render json: GardenSerializer.new(Garden.all)
     end
   end
 
   def show
+    if @garden.blank?
+      render json: { errors: "Cannot find garden with ID #{params[:id]}" }, status: :not_found
+    else
+      render json: GardenSerializer.new(@garden)
+    end
+  end
+
+  def create
+    if params[:name].blank?
+      render json: { errors: 'Name cannot be blank' }, status: :bad_request
+    else
+      garden = Garden.create(garden_params)
+      render json: GardenSerializer.new(garden), status: :created
+    end
+  end
+
+  private
+
+  def garden_params
+    params.require(:garden).permit(:name, :size)
+  end
+
+  def set_garden
+    @garden = Garden.find(params[:id])
   end
 end
