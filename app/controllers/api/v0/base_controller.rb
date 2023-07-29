eceaf7be57bdcf1586801b5b18428b8f8e8407b5
joemecha <jwmecha@gmail.com>
2023-07-29 16:02:15 -0600

    Add authentication methods and method to set current user to base_controller

--- app/controllers/api/v0/base_controller.rb
+++ app/controllers/api/v0/base_controller.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
+
 class Api::V0::BaseController < ApplicationController
   before_action :authenticate
 
   private
+
+  attr_reader :current_user
 
   def authenticate
     authenticate_user || handle_bad_authentication
   end
 
   def authenticate_user
-    @user ||= User.find_by(api_key: params[:api_key])
+    @current_user ||= User.find_by(api_key: params[:api_key])
   end
 
   def handle_bad_authentication
