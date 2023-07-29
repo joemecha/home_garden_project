31d2e522e0cc7d6f7d5e474603124ae13ce455df
joemecha <jwmecha@gmail.com>
2023-07-29 16:05:15 -0600

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_214144) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_212013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_214144) do
     t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   add_foreign_key "gardens", "users"
