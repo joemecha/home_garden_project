0c2dfeb394e28d0a362843a5baa16a14689949e4
joemecha <jwmecha@gmail.com>
2023-07-31 14:39:27 -0600

    Update routes for crops and notes to include the parent record in the route

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Rails.application.routes.draw do
       resources :gardens, only: %i[index show create update] do
         resources :locations, only: %i[index show create update]
       end
-      resources :crops, only: %i[index show create update destroy]
-      resources :notes, only: %i[index show create update destroy]
+
+      resources :locations, only: %i[none] do
+          resources :crops, only: %i[index show create update destroy]
+      end
+
+      resources :crops, only: %i[none] do
+        resources :notes, only: %i[index show create update destroy]
+      end
+
       resources :users, only: %i[create]
     end
   end
