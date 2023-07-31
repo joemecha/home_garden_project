3c232ede4f724b1e4480aea212a2f11086151889
joemecha <jwmecha@gmail.com>
2023-07-30 21:24:06 -0600

    Update routing - nest locations under gardens

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
-      resources :gardens, only: %i[index show create update]
-      resources :locations, only: %i[index show create update]
+      resources :gardens, only: %i[index show create update] do
+        resources :locations, only: %i[index show create update]
+      end
       resources :crops, only: %i[index show create update destroy]
       resources :notes, only: %i[index show create update destroy]
       resources :users, only: %i[create]
