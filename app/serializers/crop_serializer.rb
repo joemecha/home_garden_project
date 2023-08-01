082b799ef8a0e588e036597a47d1396865a0dfdf
joemecha <jwmecha@gmail.com>
2023-08-01 15:50:21 -0600

    Update crop serializer and add tests; returns days until harvest

--- app/serializers/crop_serializer.rb
+++ app/serializers/crop_serializer.rb
@@ -4,5 +4,14 @@ class CropSerializer
   include JSONAPI::Serializer
   set_type :crops
 
-  attributes :name, :variety, :days_to_maturity, :date_planted, :active
+  attributes :name, :variety, :days_to_maturity, :active
+
+  # Format the date_planted attribute as a string in "YYYY-MM-DD" format
+  attribute :date_planted do |crop|
+    crop.date_planted.strftime('%Y-%m-%d')
+  end
+  
+  attribute :days_remaining_until_harvest do |crop|
+    crop.active ? crop.days_remaining_until_harvest : 0
+  end
 end
---
+++ spec/serializers/crop_serializer_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe CropSerializer do
+  subject { CropSerializer.new(crop) }
+
+  let(:location) { build(:location) }
+  let(:crop) { create(:crop, days_to_maturity: 20, date_planted: Date.today - 10, location:) }
+
+  it 'should include the days_remaining_until_harvest attribute' do
+    days_remaining = subject.serializable_hash[:data][:attributes][:days_remaining_until_harvest]
+    expect(days_remaining).to eq(10)
+  end
+
+  it 'should include all other attributes' do
+    expect(subject.serializable_hash[:data][:attributes]).to include(
+      active: crop.active,
+      date_planted: crop.date_planted.to_s,
+      days_to_maturity: crop.days_to_maturity,
+      name: crop.name,
+      variety: crop.variety
+    )
+  end
+
+  context 'when the crop has already matured' do
+    let(:crop) { create(:crop, days_to_maturity: 20, date_planted: Date.today - 25, location:) }
+
+    it 'should have days_remaining_until_harvest as 0' do
+      expect(subject.serializable_hash[:data][:attributes][:days_remaining_until_harvest]).to eq(0)
+    end
+  end
+
+  context 'when date_planted is in the future' do
+    let(:crop) { create(:crop, days_to_maturity: 20, date_planted: Date.today + 5, location:) }
+
+    it 'should have days_remaining_until_harvest as days_to_maturity' do
+      expect(subject.serializable_hash[:data][:attributes][:days_remaining_until_harvest]).to eq(25)
+    end
+  end
+
+  context 'when the crop is inactive' do
+    let(:crop) { create(:crop, :inactive_crop, location:) }
+
+    it 'should have days_remaining_until_harvest as 0' do
+      expect(subject.serializable_hash[:data][:attributes][:days_remaining_until_harvest]).to eq(0)
+    end
+  end
+end
