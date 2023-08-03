a9c921a6f481fbaeb94d612d348026d30653a2a0
joemecha <jwmecha@gmail.com>
2023-08-03 16:35:57 -0600

    Migrations to implement Devise User and JWT (column JTI); update schema

--- db/migrate/20230803221002_add_user_reference_to_gardens.rb
+++ db/migrate/20230803221002_add_user_reference_to_gardens.rb
@@ -1,4 +1,4 @@
-class AddGardenToUser < ActiveRecord::Migration[7.0]
+class AddUserReferenceToGardens < ActiveRecord::Migration[7.0]
   def change
     add_reference :gardens, :user, foreign_key: true, null: false
   end
