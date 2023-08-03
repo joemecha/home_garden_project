d7dd71c342741b9a71ba291b986f487e4b62a35c
joemecha <jwmecha@gmail.com>
2023-08-03 16:39:16 -0600

    Update user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
-    email_address { Faker::Internet.email }
+    email { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 8, max_length: 20, mix_case: true, special_characters: true) }
-    api_key { Faker::Alphanumeric.alphanumeric(number: 8) }
 
     factory :user_with_gardens do
       transient do
