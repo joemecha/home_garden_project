63068feaf948c70a44101f667f4f38602aef26e4
joemecha <jwmecha@gmail.com>
2023-08-03 16:38:19 -0600

    Add registrations and sessions controller for auth; rack_sessions_fix concern for Rails 7 JWT workaround; related changes to application controller and users controller

--- app/controllers/api/v0/base_controller.rb
+++ app/controllers/api/v0/base_controller.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 class Api::V0::BaseController < ApplicationController
-  before_action :authenticate
+  # before_action :authenticate
 
   private
 
-  attr_reader :current_user
+  # attr_reader :current_user
 
-  def authenticate
-    @current_user ||= User.find_by(api_key: params[:api_key])
+  # def authenticate
+  #   @current_user ||= User.find_by(api_key: params[:api_key])
 
-    # If the user is not authorized, return 401 Unauthorized
-    render json: { message: 'Invalid or missing API key' }, status: :unauthorized unless @current_user
-  end
+  #   # If the user is not authorized, return 401 Unauthorized
+  #   render json: { message: 'Invalid or missing API key' }, status: :unauthorized unless @current_user
+  # end
 
   # Method to handle the 401 Not Authorized response
   def render_not_authorized(message = 'Unauthorized')
